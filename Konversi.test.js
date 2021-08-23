89f93b66e9be5de246b653f89b922873be741681
bot31 <bima.adwiyana151@gmail.com>
2021-08-23 21:59:03 +0700

    update test + image ss

--- Konversi.test.js
+++ Konversi.test.js
@@ -2,6 +2,10 @@ const {fromCelcius, fromReamur,  fromFarenheit} = require("./Konversi");
 
 test("test cel to rea", () => {
   expect(fromCelcius('rea',1)).toBe(0.8);
-  expect(fromReamur('cel',1)).toBe(1.25);
+
+});test("test far to cel", () => {;
   expect(fromFarenheit('cel',32)).toBe(0);
+
+});test("test rea to cel", () => {
+  expect(fromReamur('cel',1)).toBe(1.25);
 });
